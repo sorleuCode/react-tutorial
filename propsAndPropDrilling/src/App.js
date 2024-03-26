f042c6a031c23a3639af13fd52cf3720e3f8b0a5
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2024-03-26 09:42:57 +0100

    update

--- propsAndPropDrilling/src/App.js
+++ propsAndPropDrilling/src/App.js
@@ -1,4 +1,5 @@
 
+import AddItem from './AddItem';
 import './App.css';
 import Content from './Content';
 import Footer from './Footer';
@@ -30,6 +31,16 @@ function App() {
     },
   ])
 
+  const [newItem, setNewItem] = useState('')
+
+  const addItem = (item) => {
+    const id = items.length? items[items.length - 1].id + 1 : 1;
+    const myNewItem = {id, checked: false, item}
+    const listItems = [...items, myNewItem]
+    setItems(listItems);
+    localStorage.setItem("shoppingList", JSON.stringify(listItems)); 
+  }
+
 const handleCheck = (id) => {
 
 // console.log(`Key: ${id}`)
@@ -48,9 +59,25 @@ setItems(listItems);
 
 };
 
+const handleSubmit = (e) => {
+  e.preventDefault()
+  if (!newItem) return;
+  addItem(newItem)
+  setNewItem('');
+
+
+}
+
   return (
     <div className="App">
         <Header title= "Welcome to props"/>
+        <AddItem
+          newItem = {newItem}
+          setNewItem={setNewItem}
+          handleSubmit={handleSubmit}
+        />
+        
+
         <Content
         items={items}
         handleCheck={handleCheck}
