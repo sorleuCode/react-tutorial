5a79dc5d77a923932ced64f7853d2b41cdd1a85f
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2024-03-27 12:19:05 +0100

    update

--- color-mod/src/App.jsx
+++ color-mod/src/App.jsx
@@ -1,33 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Input from './Input'
+import Square from './Square'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [colorValue, setColorValue] = useState("");
+  const [hexValue, setHexVAlue] = useState("");
+  const [isDarkText, setIsDarkText] = useState(true);
+
+  
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <Square 
+          colorValue={colorValue}
+          hexValue={hexValue}
+          isDarkText={isDarkText}
+        />
+        <Input 
+          colorValue={colorValue}
+          setColorValue={setColorValue}
+          setHexVAlue={setHexVAlue}
+          isDarkText={isDarkText}
+          setIsDarkText={setIsDarkText}
+        />
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ color-mod/src/Input.jsx
@@ -0,0 +1,31 @@
+import colorNames from "colornames";
+import React from "react";
+
+const Input = ({
+  colorValue,
+  setColorValue,
+  setHexVAlue,
+  isDarkText,
+  setIsDarkText,
+}) => {
+  return (
+    <form onClick={(e) => e.preventDefault()}>
+      <input
+        type="text"
+        autoFocus
+        placeholder="Add Color Name"
+        required
+        value={colorValue}
+        onChange={(e) =>{
+        setColorValue(e.target.value);
+        setHexVAlue(colorNames(e.target.value))
+        }
+    }
+      />
+
+      <button onClick={() => setIsDarkText(!isDarkText)}>Toggle Text Color</button>
+    </form>
+  );
+};
+
+export default Input;
