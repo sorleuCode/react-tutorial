8c55549c9cd6bbc0897dff60452ab2994b63f95e
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2024-04-02 16:18:45 +0100

    'new-folder'

---
+++ use-effect-challenge/src/App.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import Form from "./Form";
+
+function App() {
+
+  const API_URL = 'https://jsonplaceholder.typicode.com/';
+  const [reqType, setReqType] = useState('users');
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        
+        const response = await fetch(`${API_URL}${reqType}`);
+        const data = await response.json();
+        console.log(data)
+        setItems(data)
+
+      } catch (error) {
+        
+      }
+    }
+
+    fetchItems();
+
+  }, [reqType])
+  return (
+    <div className="App">
+      <Form reqType = {reqType} setReqType={setReqType}/>
+    </div>
+  );
+}
+
+export default App;
